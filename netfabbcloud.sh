aa7d97518fa7b56851717fb644ff5edcd782c111
Miro Hrončok <miro@hroncok.cz>
2012-11-21 23:24:17 +0100

    Removed no longer needed files

--- netfabbcloud.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-USAGE="Usage: $0 file.stl [mm|inch]"
-
-
-curl --form file="$file" --form email="$email" --form units="$units" \
-	--form filerepair=1 --form termsandconditions=1 --form upload="Upload to Cloud" \
-	http://cloud.netfabb.com/index.php > /dev/null
