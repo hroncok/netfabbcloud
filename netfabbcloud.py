a7e9ea8068b76950626101decd3ade1f53568437
Miro Hrončok <miro@hroncok.cz>
2012-11-22 01:17:31 +0100

    Working e-mail parser

--- netfabbcloud.py
+++ netfabbcloud.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
-import sys, re, urllib2, MultipartPostHandler, os.path, email
+import sys, re, urllib2, MultipartPostHandler, os.path, email, time
 from datetime import datetime, timedelta
 from imapclient import IMAPClient
+
+## Put your config here
+EMAIL = 'netfabbcloud@gmail.com'	# E-mail address
+HOST = 'imap.gmail.com'				# IMAP server
+USERNAME = EMAIL					# IMAP username
+PASSWORD = '******************'		# IMAP password
+ssl = True							# Use SSL?
+UNITS = 'mm'						# mm or inch
+WAIT = 3							# How many secons wait between e-mail checks
+verbose = False						# Display actions
 
 ## Chcek if we have exactly one argument
 if len(sys.argv) != 2:
@@ -10,20 +20,14 @@ if len(sys.argv) != 2:
 	exit(1)
 
 ## Check if the argument is file
+if verbose: print 'Checking if the argument is file'
 stlfile = sys.argv[1]
 if not os.path.isfile(stlfile):
 	print '%s is not a file' % stlfile
 	exit(1)
 
-## Put your info here
-EMAIL = 'netfabbcloud@gmail.com'
-HOST = 'imap.gmail.com'
-USERNAME = EMAIL
-PASSWORD = '******************'
-ssl = True
-UNITS = 'mm'
-
 ## Send the file to Netfabb Cloud
+if verbose: print 'Sending the file to Netfabb Cloud...'
 url = 'http://cloud.netfabb.com/index.php'
 data = {}
 data['file'] = open(stlfile, 'rb')
@@ -39,29 +43,55 @@ try: page = urllib2.urlopen(request)
 except URLError, e:
 	print e.reason
 	exit(1)
+if verbose: print '...sent.'
 
 ## It seems only english letters (low or capital), numbers and dots are left in the filename
-crippledname = re.sub('[^0-9,a-z,A-Z,.]+', '', stlfile)
+crippledname = re.sub('[^0-9,a-z,A-Z,.]+', '', os.path.basename(stlfile))
+if verbose: print 'Crippled name is %s' % crippledname
+
+## Date stuff
+today = datetime.today()
+cutoff = today - timedelta(days=1)
 
 try:
-	## Connect, login and select the INBOX
-	server = IMAPClient(HOST, use_uid=True, ssl=ssl)
-	server.login(USERNAME, PASSWORD)
-	select_info = server.select_folder('INBOX')
-	## Search for relevant messages
-	messages = server.search(['FROM "cloud@netfabb.com"', 'SINCE %s' % datetime.today().strftime('%d-%b-%Y')])
-	response = server.fetch(messages, ['RFC822'])
-
-	for msgid, data in response.iteritems():
-		msg_string = data['RFC822']
-		msg = email.message_from_string(msg_string)
-		print '%s' % msg['Subject']
-		#print msg
+	## Let's find it
+	while True:
+		## Wait a bit
+		time.sleep(WAIT)
+		## Connect, login and select the INBOX
+		if verbose: print 'Connecting to IMAP'
+		server = IMAPClient(HOST, use_uid=True, ssl=ssl)
+		server.login(USERNAME, PASSWORD)
+		select_info = server.select_folder('INBOX')
+		## Search for relevant messages
+		messages = server.search(['FROM "cloud@netfabb.com"', 'SINCE %s' % cutoff.strftime('%d-%b-%Y')])
+		## Fetch the messages
+		if verbose: print 'Fetching e-mails...'
+		response = server.fetch(messages, ['RFC822'])
+		for msgid, data in response.iteritems():
+			msg = data['RFC822']
+			if verbose:
+				mail = email.message_from_string(msg)
+				print '...got e-mail: %s' % mail['Subject']
+			## Processing/Success/Failure e-mail check
+			if msg.find('"'+crippledname+'" was successfully uploaded to the system') > 0:
+				if verbose: print 'This is a processing e-mail, just delete it'
+				server.delete_messages(msgid)
+			elif msg.find('Congratulations! Your uploaded file "'+crippledname+'" was processed successfully.') > 0:
+				if verbose: print 'This is a success e-mail'
+				start = msg.find('?key=')
+				end = msg.find('&fixedfile=1')
+				print 'http://cloud.netfabb.com/download.php%s' % msg[start:end]
+				server.delete_messages(msgid)
+				exit(0)
+			elif msg.find('Unfortunately, your uploaded file "'+crippledname+'" was not processed successfully.') > 0:
+				if verbose: print 'This is a failure e-mail'
+				start = msg.find('The system logged the short error message')
+				end = msg[start:].find('If you have further questions')
+				print msg[start:start+end]
+				server.delete_messages(msgid)
+				exit(1)
 except IMAPClient.Error, e:
 	print e
 	exit(1)
 
-## Error string:
-# Unfortunately, your uploaded file "FILE" was not processed successfully. The system logged the short error message "ERROR".
-## OK string:
-# Congratulations! Your uploaded file "FILE" was processed successfully. The results are located at
